317c8df4e710df420db507984c34a0e808ba5d46
sjava-0218 <sasidhargopal_javangula@bmc.com>
2024-11-23 11:53:17 +0530

    Add files via upload

---
+++ config/ITOMStacks/helper.dsop413dv.bmc.com/properties.sh
@@ -0,0 +1,6 @@
+#helper.dsop413dv.bmc.com details
+
+export OC_USERNAME="kubeadmin" 
+export OC_PASSWORD="spcrB-s4C9C-jEIXe-fxSSp" 
+export OC_SERVER="https://api.ggok413.bmc.com:6443" 
+export NAMESPACE="matrix"
